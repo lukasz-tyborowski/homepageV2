252f57df693af6b4d4b5440b980ef85b89f8baa9
Lukasz Tyborowski <lu.tyborowski@gmail.com>
2022-08-24 12:40:13 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const firstName = 'Łukasz';
+const age = 29;
+
+console.log(firstName);
+console.log(age);
+
+console.log(`Siema, nazywam się ${firstName} i mam ${age} lat.`);
+
+alert (`Siema, nazywam się ${firstName}, mam ${age} lat. Witam w moim pierdolicznku :3`);
